13a39eb932dfd0aac88ae6284ef3f68f98679565
Arajan234 <hunanyan_ara@bk.ru>
2021-12-09 00:05:52 +0300

    First commit

--- src/part/second/volume/Main.java
+++ src/part/second/volume/Main.java
@@ -2,8 +2,16 @@ package part.second.volume;
 
 import part.second.volume.one.ThreeFriends;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         ThreeFriends threeFriends = new ThreeFriends();
+        threeFriends.nachalaputi();
+        threeFriends.chast1();
+        threeFriends.obmnaut();
+        threeFriends.podxod();
+        threeFriends.vibor();
+        threeFriends.konec();
     }
 }
